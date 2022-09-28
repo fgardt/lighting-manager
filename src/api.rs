e5de6efdc5d2912ba6bfb9fcc499f1277295ee3b
Florian Gebhardt <me@fgardt.dev>
2022-09-28 14:38:51 +0200

    use full socket

--- src/api.rs
+++ src/api.rs
@@ -12,7 +12,7 @@ pub fn run(state: State, socket: SocketAddr, runtime: &Runtime) {
     runtime.spawn(async move {
         println!("Starting API on {socket}");
 
-        serve(routes::get(state)).run(([0, 0, 0, 0], socket.port())).await;
+        serve(routes::get(state)).run(socket).await;
 
         println!("API stopped.");
     });
